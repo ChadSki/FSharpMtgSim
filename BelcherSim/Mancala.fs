44f52eb7e58de077bc7e6a75975d1a73473bcc3b
Chad Zawistowski <chad.zawistowski@gmail.com>
2015-03-21 00:56:09 -0700

    Refactor; better comments; add license

--- BelcherSim/Mancala.fs
+++ BelcherSim/Mancala.fs
@@ -4,126 +4,139 @@ open System
 open System.Collections.Generic
 open System.IO
 
-type Slot = { count:int; max:int }
 type Board = Slot list
+and Slot = { count:int; max:int }
 
 let PrettyPrintBoard (board:Board) =
     board |> List.map (fun slot -> if slot.max = 0 then "" // ignore non-participating slots
                                    else (sprintf "%d " slot.count))
           |> List.reduce (+)
 
-let InitialCombination (emptyBoard:Board) totalBeads : Board =
-    let numBeads = ref totalBeads
+let InitialCombination (emptyBoard:Board) totalTokens : Board =
+    // mutable reference represents the global token pool
+    let numTokens = ref totalTokens
 
-    // Fill the slots in sequence order (left to right)
+    // Distribute the tokens starting with the leftmost slots.
     let filledBoard =
         emptyBoard |> List.map (fun slot ->
-            let remainderIfGreedy = !numBeads - slot.max
+            let remainderIfGreedy = !numTokens - slot.max
             match remainderIfGreedy with
 
             // We want more than is available. Take what is left.
-            | remainder when remainder < 0 -> let allThatsLeft = !numBeads
-                                              numBeads := 0
+            | remainder when remainder < 0 -> let allThatsLeft = !numTokens
+                                              numTokens := 0
                                               { count = allThatsLeft
                                                 max = slot.max }
 
             // There's enough to take our fill!
-            | _ -> numBeads := remainderIfGreedy
+            | _ -> numTokens := remainderIfGreedy
                    { count = slot.max
                      max = slot.max })
 
-    if !numBeads > 0 then raise (new ArgumentException "Too many beads to fit in these slots")
+    if !numTokens > 0 then raise (new ArgumentException "Too many tokens to fit in these slots")
     else filledBoard
 
 type OverflowResult = SpilledOffEnd | ValidState
 
+type MutBoard = MutSlot list
+and MutSlot =
+    { mutable counter:int; max:int }
+    // Convenience operators for manipulating the counter of a MutSlot
+    static member (+=) (ms : MutSlot, a) = ms.counter <- ms.counter + a
+    static member (-=) (ms : MutSlot, a) = ms.counter <- ms.counter - a
+    static member (!=) (ms : MutSlot, a) = ms.counter <- a
+
 let rec NextCombination (oldBoard:Board) : Board option =
     // Mutable copy of the deck
-    let mutDeck = new List<Slot>(oldBoard)
+    let mutDeck = oldBoard |> List.map (fun slot -> { counter = slot.count; max = slot.max })
     let lastPos = oldBoard.Length - 1
 
-    // The algorithm often involves 'lifting' beads up to move them around. This var represents the
-    // number of beads currently lifted. By the time we return (or recurse), lifted should be empty
-    // so that we aren't losing any beads.
+    // The algorithm often involves 'lifting' tokens up to move them around. This var represents the
+    // number of tokens currently lifted. By the time we return (or recurse), lifted should be empty
+    // so that we aren't losing any tokens.
     let lifted = ref 0
 
-    // First lift all the beads from the last slot
-    lifted := mutDeck.[lastPos].count
-    mutDeck.[lastPos] <- { count = 0
-                           max = mutDeck.[lastPos].max }
+    // First lift all the tokens from the last slot
+    lifted := mutDeck.[lastPos].counter
+    mutDeck.[lastPos] != 0
 
-    // Find the right-most bead that isn't lifted
+    // Find the right-most token that isn't lifted
     match mutDeck |> Seq.toList
                   |> List.rev
-                  |> List.tryFindIndex (fun slot -> slot.count > 0) with
+                  |> List.tryFindIndex (fun slot -> slot.counter > 0) with
 
-    | None -> None  // No more legal deck positions; sequence complete
+    // Can't find a token; no more legal deck positions; sequence complete.
+    | None -> None
+
+    // Slide tokens into next combination
     | Some indexFromBack ->
-        // Position of the right-most bead
-        let pos = ref ((oldBoard |> Seq.length) - 1 - indexFromBack)  // invert, since we searched while reversed
+        // Position of the right-most token
+        let pos = ref ((oldBoard |> Seq.length) - 1 - indexFromBack)  // invert, since we searched
+                                                                      // while reversed
 
-        // Pick up a single bead, move to the right, and drop all beads
-        mutDeck.[!pos] <- { count = mutDeck.[!pos].count - 1
-                            max = mutDeck.[!pos].max }
-        pos := !pos + 1
-        mutDeck.[!pos] <- { count = mutDeck.[!pos].count + 1 + !lifted
-                            max = mutDeck.[!pos].max }
+        mutDeck.[!pos] -= 1              // pick up a single token
+        pos := !pos + 1                  // move one slot to the right
+        mutDeck.[!pos] += (1 + !lifted)  // drop all tokens
         lifted := 0
 
-        // Recursively "overflow" beads to the right until no slots hold more than their maximum.
+        // Recursively "overflow" tokens to the right until each slots is within its maximum.
         let rec overflow () =
-            if !pos > lastPos then SpilledOffEnd
-            else
-                let curr = mutDeck.[!pos].count
-                let ceiling = mutDeck.[!pos].max
-                if (curr > ceiling || curr = ceiling && !lifted > 0) then
-                    // Position is on the board, but the number of beads will be over the maximum
-                    // unless we spill into the next slot.
-                    lifted := !lifted + mutDeck.[!pos].count - ceiling
-                    mutDeck.[!pos] <- { count = ceiling
-                                        max = mutDeck.[!pos].max }
+            if (mutDeck.[!pos].counter <= mutDeck.[!pos].max)
+            then ValidState
+            else  // We're on the board, but the number of tokens is over the maximum and
+                  // we need to spill into the next slot.
 
-                    pos := !pos + 1
-                    if !pos > lastPos then SpilledOffEnd
-                    else
-                        mutDeck.[!pos] <- { count = mutDeck.[!pos].count + !lifted
-                                            max = mutDeck.[!pos].max }
-                        lifted := 0
-                        overflow ()
-                else ValidState
+                // Lift extra tokens
+                lifted := mutDeck.[!pos].counter - mutDeck.[!pos].max
+                mutDeck.[!pos] != mutDeck.[!pos].max
+
+                // Try to deposit them one slot to the right
+                pos := !pos + 1
+                if !pos <= lastPos
+                then
+                    // Okay! Deposit tokens.
+                    mutDeck.[!pos] != !lifted
+                    lifted := 0
+
+                    // Recurse, in case this slot also needs to overflow.
+                    overflow ()
+
+                else
+                    // No more slots! Position is off the board. Deposit unaccounted-for
+                    // tokens in the last slot.
+                    mutDeck.[lastPos] += !lifted
+                    lifted := 0
+                    SpilledOffEnd
 
         match overflow () with
-
-        // We have a legal deck position
-        | ValidState -> Some (mutDeck |> Seq.toList)
-
+        // We have a legal deck position! Convert back to immutable form.
+        | ValidState -> Some (mutDeck |> List.map (fun mutSlot -> { count = mutSlot.counter
+                                                                    max = mutSlot.max }))
         // Overflowed! Speed ahead to the next valid combination
         | SpilledOffEnd ->
-
-            // First, deposit beads at the end
-            mutDeck.[lastPos] <- { count = mutDeck.[lastPos].count + !lifted
-                                   max = mutDeck.[lastPos].max }
-            lifted := 0
-
-            // Pick up all beads on the cusp of overflow
+            // Pick up all tokens on the cusp of overflow, starting from the far right and
+            // iterating to the left.
             pos := lastPos
-            while mutDeck.[!pos].count >= mutDeck.[!pos].max do
-                lifted := !lifted + mutDeck.[!pos].count
-                mutDeck.[!pos] <- { count = 0
-                                    max = mutDeck.[!pos].max }
+            while mutDeck.[!pos].counter >= mutDeck.[!pos].max do
+                // Pick up tokens
+                lifted := !lifted + mutDeck.[!pos].counter
+                mutDeck.[!pos] != 0
+
+                // Move one slot to the left
                 pos := !pos - 1
 
-            // Deposit them at the end, too
-            mutDeck.[lastPos] <- { count = mutDeck.[lastPos].count + !lifted
-                                   max = mutDeck.[lastPos].max }
+            // Deposit all overflowed tokens at the end
+            mutDeck.[lastPos] += !lifted
             lifted := 0
 
-            // This combination isn't legal, but we can find the next legal combo from it.
-            NextCombination (mutDeck |> Seq.toList)
+            // This combination isn't legal, but we can find the next legal combo
+            // by recursing on it.
+            NextCombination (mutDeck |> List.map (fun mutSlot -> { count = mutSlot.counter
+                                                                   max = mutSlot.max }))
 
 
 // Given a board, iterate through all possible combinations
-let MancalaSequence emptyBoard numBeads =
+let MancalaSequence emptyBoard numTokens =
     // #region Debug
     let prevElapsedSeconds = ref 0.
     let chunkSize = 100000
@@ -131,15 +144,14 @@ let MancalaSequence emptyBoard numBeads =
     let stopWatch = System.Diagnostics.Stopwatch.StartNew()
     let outFile = new StreamWriter("output-mtg.txt")
     let myprint (str:string) =
-        printfn "%s" str
         outFile.WriteLine(str)
-        outFile.Flush()
+        printfn "%s" str
 
     myprint (sprintf "Started at %s" (DateTime.Now.ToString()))
     // #endregion
 
     seq {
-        let firstBoard = InitialCombination emptyBoard numBeads
+        let firstBoard = InitialCombination emptyBoard numTokens
         yield firstBoard
         let currBoard = ref firstBoard
         total := !total + 1
