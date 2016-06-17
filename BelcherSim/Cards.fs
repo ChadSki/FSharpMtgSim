22c7eb79378cfded087d80dbe50b4b072858d8e0
Chad Zawistowski <chad.zawistowski@gmail.com>
2016-06-16 23:31:20 -0700

    Implement ManaAmount and stub cost function

--- BelcherSim/Cards.fs
+++ BelcherSim/Cards.fs
@@ -15,3 +15,51 @@ type Card = | BurningWish       | ChancellorOfTheTangle | ChromeMox     | Desper
 let allCards = FSharpType.GetUnionCases typeof<Card>
                |> Array.map(fun caseInfo -> FSharpValue.MakeUnion(caseInfo, [||]) :?> Card)
                |> Array.toList
+
+
+// Represents a quantity of mana
+type ManaAmount =
+    { red:int; white:int; green:int; blue:int; black:int; colorless:int }
+
+    static member (+) (left:ManaAmount, right:ManaAmount) =
+        { red       = left.red       + right.red
+          white     = left.white     + right.white
+          green     = left.green     + right.green
+          blue      = left.blue      + right.blue
+          black     = left.black     + right.black
+          colorless = left.colorless + right.colorless }
+
+    static member (-) (left:ManaAmount, right:ManaAmount) =
+        { red       = left.red       - right.red
+          white     = left.white     - right.white
+          green     = left.green     - right.green
+          blue      = left.blue      - right.blue
+          black     = left.black     - right.black
+          colorless = left.colorless - right.colorless }
+
+let cost = function
+| BurningWish ->           { red=0; white=0; green=0; blue=0; black=0; colorless=0 }
+| ChancellorOfTheTangle -> { red=0; white=0; green=0; blue=0; black=0; colorless=0 }
+| ChromeMox ->             { red=0; white=0; green=0; blue=0; black=0; colorless=0 }
+| DesperateRitual ->       { red=0; white=0; green=0; blue=0; black=0; colorless=0 }
+| ElvishSpiritGuide ->     { red=0; white=0; green=0; blue=0; black=0; colorless=0 }
+| EmptyTheWarrens ->       { red=0; white=0; green=0; blue=0; black=0; colorless=0 }
+| GitaxianProbe ->         { red=0; white=0; green=0; blue=0; black=0; colorless=0 }
+| GoblinCharbelcher ->     { red=0; white=0; green=0; blue=0; black=0; colorless=0 }
+| LandGrant ->             { red=0; white=0; green=0; blue=0; black=0; colorless=0 }
+| LionsEyeDiamond ->       { red=0; white=0; green=0; blue=0; black=0; colorless=0 }
+| LotusPetal ->            { red=0; white=0; green=0; blue=0; black=0; colorless=0 }
+| Manamorphose ->          { red=0; white=0; green=0; blue=0; black=0; colorless=0 }
+| PyreticRitual ->         { red=0; white=0; green=0; blue=0; black=0; colorless=0 }
+| RiteOfFlame ->           { red=0; white=0; green=0; blue=0; black=0; colorless=0 }
+| SeethingSong ->          { red=0; white=0; green=0; blue=0; black=0; colorless=0 }
+| SerumPowder ->           { red=0; white=0; green=0; blue=0; black=0; colorless=0 }
+| SimianSpiritGuide ->     { red=0; white=0; green=0; blue=0; black=0; colorless=0 }
+| StreetWraith ->          { red=0; white=0; green=0; blue=0; black=0; colorless=0 }
+| Taiga ->                 { red=0; white=0; green=0; blue=0; black=0; colorless=0 }
+| TinderWall ->            { red=0; white=0; green=0; blue=0; black=0; colorless=0 }
+
+let x = cost BurningWish
+let y = cost ChromeMox
+let z = x + y
+let w = x - y
