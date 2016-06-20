4fdc2a676644b50e41c5899e3f739deab34156ec
Chad Zawistowski <chad.zawistowski@gmail.com>
2016-06-19 17:10:29 -0700

    Mana cost improvements

--- BelcherSim/Mana.fs
+++ BelcherSim/Mana.fs
@@ -18,28 +18,25 @@ type ManaAmount =
 
 let noMana = { red=0; green=0; colorless=0 }
 
-// For when a card isn't meant to be cast normally
-let prohibitiveCost = { red=99; green=99; colorless=99; }
-
 let Cost = function
 | BurningWish ->           { red=1; green=0; colorless=1 }
-| ChancellorOfTheTangle -> prohibitiveCost
+| ChancellorOfTheTangle -> { red=0; green=3; colorless=4 }
 | ChromeMox ->             noMana
 | DesperateRitual ->       { red=1; green=0; colorless=1 }
-| ElvishSpiritGuide ->     prohibitiveCost
+| ElvishSpiritGuide ->     { red=0; green=1; colorless=2 }
 | EmptyTheWarrens ->       { red=1; green=0; colorless=3 }
-| GitaxianProbe ->         prohibitiveCost
+| GitaxianProbe ->         { red=0; green=0; colorless=99 } // Not to be played normally
 | GoblinCharbelcher ->     { red=0; green=0; colorless=4 }
 | LandGrant ->             { red=0; green=1; colorless=1 }
 | LionsEyeDiamond ->       noMana
 | LotusPetal ->            noMana
-| Manamorphose ->          { red=0; green=0; colorless=2 } // R/G mana makes this effectively colorless
+| Manamorphose ->          { red=0; green=0; colorless=2 }  // R/G mana makes this effectively colorless
 | PyreticRitual ->         { red=1; green=0; colorless=1 }
 | RiteOfFlame ->           { red=1; green=0; colorless=0 }
 | SeethingSong ->          { red=1; green=0; colorless=2 }
-| SerumPowder ->           prohibitiveCost
-| SimianSpiritGuide ->     prohibitiveCost
-| StreetWraith ->          prohibitiveCost
+| SerumPowder ->           { red=0; green=0; colorless=3 }
+| SimianSpiritGuide ->     { red=1; green=0; colorless=2 }
+| StreetWraith ->          { red=0; green=0; colorless=99 } // Not to be played normally
 | Taiga ->                 noMana
 | TinderWall ->            { red=0; green=1; colorless=0 }
 
@@ -49,13 +46,14 @@ type ManaColor =
      | Colorless
      | Multicolor
 
-let Color card =
-    let manaCost = Cost card
-    match manaCost.red > 0 with
-    | true -> match manaCost.green > 0 with
-              | true -> Multicolor
-              | false -> Red
+let Color = function
+| Manamorphose -> Multicolor  // Special-cased because we counted the R/G mana as effectively colorless
+| card -> let manaCost = Cost card
+          match manaCost.red > 0 with
+          | true -> match manaCost.green > 0 with
+                    | true -> Multicolor
+                    | false -> Red
 
-    | false -> match manaCost.green > 0 with
-                | true -> Green
-                | false -> Colorless
+          | false -> match manaCost.green > 0 with
+                     | true -> Green
+                     | false -> Colorless
