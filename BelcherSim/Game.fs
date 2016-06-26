990f027593f5373bd4c0f4dd980e45f3260f9200
Chad Zawistowski <chad.zawistowski@gmail.com>
2016-06-26 03:14:39 -0700

    Switch from refs to plain mutable state

--- BelcherSim/Game.fs
+++ BelcherSim/Game.fs
@@ -7,38 +7,40 @@ open CardGroup
 open Mana
 open ChromeMox
 
+// Big 'ol mutable wad of state.
 type GameState(_library: Card list,
                _hand: Card list,
                _battlefield: (Card * bool) list,  // Card, and whether it's tapped
-               _moxen: (ManaColor * bool) list,  // Color, and whether it's been used
+               _moxen: (ManaColor * bool) list,  // Imprinted Chrome Mox colors, and whether they've been spent
                _graveyard: Card list,
                _mana: ManaAmount) =
 
-    member x.Library = ref _library
-    member x.Hand = ref _hand
-    member x.Battlefield = ref _battlefield
-    member x.Moxen = ref _moxen
-    member x.Graveyard = ref _graveyard
-    member x.Mana = ref _mana
+    member val Library = _library with get, set
+    member val Hand = _hand with get, set
+    member val Battlefield = _battlefield with get, set
+    member val Moxen = _moxen with get, set
+    member val Graveyard = _graveyard with get, set
+    member val Mana = _mana with get, set
+    member this.Clone() = this.MemberwiseClone() :?> GameState
 
 // General "what should I do next" function, can be called recursively.
-let TakeAction (gs:GameState) =
+let rec TakeAction (gs:GameState) =
 
     // Play Chrome Mox if possible
-    if !(gs.Hand) |> List.exists ((=) ChromeMox) then
+    if gs.Hand |> List.exists ((=) ChromeMox) then
 
         // Remove from hand and put in play
-        gs.Hand := RemoveFromCardGroup !(gs.Hand) ChromeMox
-        gs.Battlefield := (ChromeMox, false) :: !(gs.Battlefield)
+        gs.Hand <- RemoveFromCardGroup gs.Hand ChromeMox
+        gs.Battlefield <- (ChromeMox, false) :: gs.Battlefield
 
         // If we imprinted a card, add its color to the list of moxen
-        match ChooseImprint !(gs.Hand) with
+        match ChooseImprint gs.Hand with
         | None -> ()
         | Some imprint ->
-            gs.Hand := RemoveFromCardGroup !(gs.Hand) imprint
-            gs.Moxen := (Color (Cost imprint), false) :: !(gs.Moxen)
+            gs.Hand <- RemoveFromCardGroup gs.Hand imprint
+            gs.Moxen <- (Color (Cost imprint), false) :: gs.Moxen
 
-        true
+        TakeAction gs
     else
         false
 
